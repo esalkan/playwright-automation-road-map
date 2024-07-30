8cc08c038c3bba5f527281abc6ac8fd08dc35075
Eyüp Sabri ALKAN <esalkan@icloud.com>
2024-07-30 04:09:43 +0300

    Update 02_InitializeNewContext_NewBrowser.spec.js
    newPage();

--- tests/UIBasics/02_InitializeNewContext_NewBrowser.spec.js
+++ tests/UIBasics/02_InitializeNewContext_NewBrowser.spec.js
@@ -20,3 +20,30 @@ test('Initialize newContext', async ({ browser }) => {
     // Varsayılan chromium
 });
 
+// Initialize browser 
+// async ({ browser }) => browser object is passed to the test block as a parameter to use it.
+// async ({ browser }) => browser nesnesi, test bloğunda kullanılmak üzere bir parametre olarak aktarılır.
+test('Initialize newPage', async ({ browser }) => {
+
+    // Creates a new browser tab or page in an existing browser context. 
+    // Shares the same session and storage (cookies, local storage) with other pages in the same context. 
+    
+    const page = await browser.newPage();
+    
+    // Mevcut bir tarayıcı bağlamında yeni bir tarayıcı sekmesi veya sayfası oluşturur.
+    // Aynı bağlamdaki diğer sayfalarla aynı oturumu ve depolamayı (çerezler, yerel depolama) paylaşır.
+
+    // Default chromium
+    // Varsayılan chromium
+});
+
+/*
+    - browser.newPage() creates a new tab within the same session, while browser.
+    - newContext() creates a completely isolated browser session, 
+    - useful for testing scenarios that require separate user sessions.
+
+    - browser.newPage() aynı oturum içinde yeni bir sekme oluştururken, browser.
+    - newContext() tamamen yalıtılmış bir tarayıcı oturumu oluşturur, 
+    - ayrı kullanıcı oturumları gerektiren senaryoları test etmek için kullanışlıdır.
+*/
+
